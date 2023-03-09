6ac71ed57b628851e54775b7393034932050fbac
Sienna <remicknsisi@gmail.com>
2023-03-09 12:51:09 -0500

    commit tree c975a6471702e5c33a425f16e52fd190a4b3aa49

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves arr
+  arr.to_enum.with_index { |n, index| puts "#{index + 1}. #{n}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet arr
+  arr.map do |e|
+    e.split('').first.upcase() + e.slice(1, arr.length - 1) + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls arr
+  arr.any? {|element| element.split('').count > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese arr
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  if arr.include?(cheese_types[0])
+    "cheddar"
+  elsif arr.include?(cheese_types[1])
+    "gouda"
+  elsif arr.include?(cheese_types[2])
+    "camembert"
+  else
+    nil
+  end
+
 end
